c7db5d07eddafcaf8897324c72f5c5782a7c4b11
Oskar Jarosz <oscarjarosz@gmail.com>
2017-11-07 00:35:48 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('serve', ['sass'], function () {
+	browserSync({
+		server: {
+			index: 'index.html'
+		}
+	});
+
+	gulp.watch('*.html', ['reload']);
+	gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+
+gulp.task('sass', function () {
+	return gulp.src('scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+
+		.pipe(autoprefixer({
+			browsers: ['last 3 versions']
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
